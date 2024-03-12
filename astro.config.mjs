5d561bc6d0f47461616f009eb6a3609105253fc6
Giulio Zanchetta <zank@outlook.it>
2024-03-12 14:14:49 +0100

    Added React and Markdoc integrations to Astro config
    In this update, we've added React and Markdoc as integrations in the Astro configuration. We've also included new dependencies in package.json for Keystatic's Astro and Core packages. Additionally, we updated the TypeScript configuration to support JSX syntax with a source of 'react'.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), markdoc()]
+});
